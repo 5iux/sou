ccd9cc0f09933776b9f2d2f9055944827a74ef7c
D.Young <13775651+5iux@users.noreply.github.com>
2019-06-28 10:45:49 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -3,15 +3,17 @@
 error_reporting(0);
 $t=htmlspecialchars($_POST["t"]);
 $q=htmlspecialchars($_POST["q"]);
+$v=htmlspecialchars($_GET["v"]);
 if ($q==""||$q==null) {
 }else{
   if ($t=="b"){
-     echo'<script>window.location.href="//www.baidu.com/s?ie=utf-8&word='.$q.'"</script>';
+    echo'<script>window.location.href="//www.baidu.com/s?ie=utf-8&word='.$q.'"</script>';
   }else if($t=="g"){
-      echo'<script>window.location.href="https://www.google.com/search?hl=zh&q='.$q.'"</script>';
+    echo'<script>window.location.href="https://www.google.com/search?hl=zh&q='.$q.'"</script>';
   }else{
-  		echo'<script>window.location.href="https://www.google.com/search?hl=zh&q='.$q.'"</script>';
-  	
+    //默认谷歌,切换百度可以注释掉下面的google，换成下面的百度
+ 		echo'<script>window.location.href="https://www.google.com/search?hl=zh&q='.$q.'"</script>';
+    //echo'<script>window.location.href="//www.baidu.com/s?ie=utf-8&word='.$q.'"</script>';
   }
 };
 ?>
@@ -35,7 +37,16 @@ if ($q==""||$q==null) {
   <title>简单搜索</title>
   <?php echo'<link href="style.css?t='.date("ymdhi").'" rel="stylesheet">'  ?>
   <script src="https://cdnjs.loli.net/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <link rel="stylesheet" href="//at.alicdn.com/t/font_1230786_ku11bmsaw1j.css">
+  <link rel="stylesheet" href="//at.alicdn.com/t/font_1230786_8056l7kbz5a.css">
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2530a31105f02a18bdca86cb33da26ec";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
 </head>
 
 <body>
@@ -59,6 +70,17 @@ if ($q==""||$q==null) {
             <li><a rel="nofollow" href="https://www.twitter.com" target="_blank"><i class="iconfont icon-twitter" style="color: #00bcff;"></i>Twitter</a></li>
             <li><a rel="nofollow" href="https://www.facebook.com" target="_blank"><i class="iconfont icon-facebook" style="color: #4267b2;"></i>Facebook</a></li>
           <!------>
+            <li class="title"><i class="iconfont icon-shipin"></i> 视频媒体</li>
+            <li><a rel="nofollow" href="https://www.youtube.com/" target="_blank"><i class="iconfont icon-youtube" style="color:#f03;"></i>Youtube</a></li>
+            <li><a rel="nofollow" href="https://v.qq.com/" target="_blank"><i class="iconfont icon-tengxunshipin" style="color:#2a0;"></i>腾讯视频</a></li>
+            <li><a rel="nofollow" href="https://www.youku.com/" target="_blank"><i class="iconfont icon-youku" style="color:#09e;"></i>优酷</a></li>
+            <li><a rel="nofollow" href="https://www.iqiyi.com/" target="_blank"><i class="iconfont icon-aiqiyi" style="color:#1d0;"></i>爱奇艺</a></li>
+            <li><a rel="nofollow" href="http://www.zmz2019.com/" target="_blank"><i class="iconfont icon-zimu" style="color: #067;"></i>字幕组</a></li>
+            <li><a rel="nofollow" href="http://www.acfun.cn/index.html" target="_blank"><i class="iconfont icon-acfun" style="color:#f33;"></i>ACFUN</a></li>
+            <li><a rel="nofollow" href="https://www.bilibili.com/" target="_blank"><i class="iconfont icon-bili" style="color:#09e;"></i>哔哩哔哩</a></li>
+            <li><a rel="nofollow" href="https://www.yunbtv.com/" target="_blank"><i class="iconfont icon-yunbo" style="color:#0bf;"></i>云播影视</a></li>
+            <li><a rel="nofollow" href="https://music.yyv.me/" target="_blank"><i class="iconfont icon-yinle" style="color:#f4a;"></i>音乐解析</a></li>
+          <!------>
             <li class="title"><i class="iconfont icon-gouwuche"></i> 购物</li>
             <li><a rel="nofollow" href="https://www.taobao.com" target="_blank"><i class="iconfont icon-taobao" style="color: #ff6019;"></i>淘宝网</a></li>
             <li><a rel="nofollow" href="https://dyartstyle.com/juhuasuan/" target="_blank"><i class="iconfont icon-juhuasuan" style="color: #E61414;"></i>聚划算</a></li>
@@ -69,19 +91,7 @@ if ($q==""||$q==null) {
             <li><a rel="nofollow" href="http://you.163.com/" target="_blank"><i class="iconfont icon-yanxuan" style="color: #B4A078;"></i>网易严选</a></li>
             <li><a rel="nofollow" href="https://www.amazon.cn/" target="_blank"><i class="iconfont icon-amazon" style="color: #fc9b18;"></i>亚马逊</a></li>
             <li><a rel="nofollow" href="http://www.dangdang.com/" target="_blank"><i class="iconfont icon-dangdang" style="color: #ff2832;"></i>当当</a></li>
-            <li><a rel="nofollow" href="https://dyartstyle.com/quan/" target="_blank"><i class="iconfont icon-youhuiquan" style="color: #f03;"></i>搜优惠券</a></li>
-          <!------>
-            <li class="title"><i class="iconfont icon-shipin"></i> 视频媒体</li>
-            <li><a rel="nofollow" href="https://www.youtube.com/" target="_blank"><i class="iconfont icon-youtube" style="color:#f03;"></i>Youtube</a></li>
-            <li><a rel="nofollow" href="https://v.qq.com/" target="_blank"><i class="iconfont icon-tengxunshipin" style="color:#2a0;"></i>腾讯视频</a></li>
-            <li><a rel="nofollow" href="https://www.youku.com/" target="_blank"><i class="iconfont icon-youku" style="color:#09e;"></i>优酷</a></li>
-            <li><a rel="nofollow" href="https://www.iqiyi.com/" target="_blank"><i class="iconfont icon-aiqiyi" style="color:#1d0;"></i>爱奇艺</a></li>
-            <li><a rel="nofollow" href="http://www.zmz2019.com/" target="_blank"><i class="iconfont icon-zimu" style="color: #067;"></i>字幕组</a></li>
-            <li><a rel="nofollow" href="http://www.acfun.cn/index.html" target="_blank"><i class="iconfont icon-acfun" style="color:#f33;"></i>ACFUN</a></li>
-            <li><a rel="nofollow" href="https://www.bilibili.com/" target="_blank"><i class="iconfont icon-bili" style="color:#09e;"></i>哔哩哔哩</a></li>
-            <li><a rel="nofollow" href="https://www.yunbtv.com/" target="_blank"><i class="iconfont icon-yunbo" style="color:#0bf;"></i>云播影视</a></li>
-            <li><a rel="nofollow" href="/tv/" target="_blank"><i class="iconfont icon-dianshi" style="color:#9ae;"></i>直播电视</a></li>
-            <li><a rel="nofollow" href="https://music.yyv.me/" target="_blank"><i class="iconfont icon-yinle" style="color:#f4a;"></i>音乐解析</a></li>
+            <li><a rel="nofollow" href="https://dyartstyle.com/shop/" target="_blank"><i class="iconfont icon-youhuiquan" style="color: #f03;"></i>吾爱淘</a></li>
           <!------>
             <li class="title"><i class="iconfont icon-sheji"></i> 设计视觉</li>
             <li><a rel="nofollow" href="https://web.yyv.me/" target="_blank"><i class="iconfont icon-daohang1"></i>设计导航</a></li>
@@ -95,11 +105,13 @@ if ($q==""||$q==null) {
             <li><a rel="nofollow" href="https://uiiiuiii.com/" target="_blank"><i class="iconfont icon-jiaocheng" style="color:#0aa;"></i>优设教程</a></li>
             <!------>
             <li class="title"><i class="iconfont icon-ai-tool"></i> 工具</li>
+            <li><a rel="nofollow" href="https://5iux.github.io/ip/" target="_blank"><i class="iconfont icon-wangluo" style="color: #02f;"></i>IP查询</a></li>
             <li><a rel="nofollow" href="https://translate.google.cn/?hl=zh-CN" target="_blank"><i class="iconfont icon-fanyi" style="color: #02f;"></i>谷歌翻译</a></li>
+            <li><a rel="nofollow" href="http://www.slimego.cn/" target="_blank"><i class="iconfont icon-shilaimu" style="color: #0f89c2;"></i>史莱姆</a></li>
             <li><a rel="nofollow" href="https://gugeji.com/" target="_blank"><i class="iconfont icon-google" style="color:#4285f4"></i>镜像</a></li>
             <li><a rel="nofollow" href="https://feedly.com" target="_blank"><i class="iconfont icon-feedly" style="color: #11dd00;"></i>Feedly</a></li>         
             <li><a rel="nofollow" href="https://pan.baidu.com" target="_blank"><i class="iconfont icon-baiduyun" style="color: #148bfe;"></i>百度网盘</a></li>
-            <li><a rel="nofollow" href="https://www.baiduwp.com" target="_blank"><i class="iconfont icon-cloud-download" style="color: #148bfe;"></i>盘在线解析</a></li>
+            <li><a rel="nofollow" href="https://www.baiduwp.com" target="_blank"><i class="iconfont icon-cloud-download" style="color: #148bfe;"></i>网盘解析</a></li>
             <li><a rel="nofollow" href="https://www.mdeditor.com/" target="_blank"><i class="iconfont icon-md" style="color:#09f;"></i>MD编辑器</a></li>
             <li><a rel="nofollow" href="https://miku.tools/" target="_blank"><i class="iconfont icon-ai-tool"></i>Miku工具</a></li>
             <li><a rel="nofollow" href="http://cubic-bezier.com" target="_blank"><i class="iconfont icon-quxian" style="color: #f08;"></i>贝赛尔曲线</a></li>
@@ -122,7 +134,7 @@ if ($q==""||$q==null) {
             <li><a rel="nofollow" href="https://www.swiper.com.cn/" target="_blank"><i class="iconfont icon-S" style="color:#065fe3;"></i>Swiper</a></li>
         </ul>
     </div>
-    <div class="mywth"><iframe scrolling="no" src="https://tianqiapi.com/api.php?style=ta&skin=orange&fontsize=13" frameborder="0" width="260" height="24" allowtransparency="true"></iframe></div>
+    <div id="tp-weather-widget" class="mywth"></div>
     <div id="content">
         <div class="con">
             <div class="shlogo"></div>
@@ -170,24 +182,50 @@ if ($q==""||$q==null) {
             $("#menu").click(function(event) {
                 $(this).toggleClass('on');
                 $(".list").toggleClass('closed');
-                $(".mywth").toggleClass('hidden');
+                $("#tp-weather-widget").toggleClass('hidden');
             });
             $("#content").click(function(event) {
                 $(".on").removeClass('on');
                 $(".list").addClass('closed');
-                $(".mywth").removeClass('hidden');
+                $("#tp-weather-widget").removeClass('hidden');
             });
             /*
-            $("iframe").click(function(event) {
+               为界面干净无广告，移除天气插件版权信息
+               天气插件版权备注到这里：心知天气版权所有；
+               需要请到：https://www.seniverse.com/widget/  
+            */
+            $("#tp-weather-widget").mouseover(function(event) {
+                $("span:contains('心知天气')").parent().remove();
+            });
+            $(".mywth").click(function(event) {
               var wt=$("body").width();
               if(wt>640){
                 window.location.href="https://tianqi.qq.com/";
               }else{
-                window.location.href="https://apip.weatherdt.com/h5.html?id=zehTqHpWFP";
+                window.location.href="https://apip.weatherdt.com/h5.html?id=JrCSiXLssE";
               }
-            });*/
+            });
         });
         </script>
+
+<!--天气插件开始-->
+<script>(function(T,h,i,n,k,P,a,g,e){g=function(){P=h.createElement(i);a=h.getElementsByTagName(i)[0];P.src=k;P.charset="utf-8";P.async=1;a.parentNode.insertBefore(P,a)};T["ThinkPageWeatherWidgetObject"]=n;T[n]||(T[n]=function(){(T[n].q=T[n].q||[]).push(arguments)});T[n].l=+new Date();if(T.attachEvent){T.attachEvent("onload",g)}else{T.addEventListener("load",g,false)}}(window,document,"script","tpwidget","//widget.seniverse.com/widget/chameleon.js"))</script>
+<script>tpwidget("init", {
+    "flavor": "slim",
+    "location": "WWE0TGW4PX6N",
+    "geolocation": "enabled",
+    "language": "zh-chs",
+    "unit": "c",
+    "theme": "chameleon",
+    "container": "tp-weather-widget",
+    "bubble": "enabled",
+    "alarmType": "badge",
+    "uid": "UFFFC70B02",
+    "hash": "8ca5fdc919e446872f170fb32648c78c"
+});
+tpwidget("show");</script>
+<!--天气插件结束-->
+
 
 
         <div class="foot">© 2016-<?php echo date("Y") ?> by <a href="https://yyv.me/">歪歪喂</a> . All rights reserved.</div>
@@ -196,7 +234,7 @@ if ($q==""||$q==null) {
 作者:D.Young
 主页：https://yyv.me/
 github：https://github.com/5iux/sou
-日期：2019-06-18
+日期：2019-06-11
 版权所有，请勿删除
 -->
 </body>
