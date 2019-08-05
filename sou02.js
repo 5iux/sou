d92ba1080f823adc5af394467ff6166ad545f229
D.Young <13775651+5iux@users.noreply.github.com>
2019-08-05 15:01:47 +0800

    Update sou02.js

--- sou02.js
+++ sou02.js
@@ -71,19 +71,19 @@ $.ajax({
     success: function(res) {
         uptime = res.update_time.substring(11);
         uptime = uptime.substring(0, uptime.length - 3);
-        $('.mywth').append(res.city + ' <img class="wea" src="https://xuesax.com/tianqiapi/skin/peach/' + res.data[0].wea_img + '.png"> ' + res.data[0].wea + ' ' + res.data[0].tem1 + '/' + res.data[0].tem2 + ' ' + res.data[0].air_level);
+        $('.mywth').append(res.city + ' <img class="wea" src="https://www.tianqiapi.com/static/skin/yahoo/' + res.data[0].wea_img + '.png"> ' + res.data[0].wea + ' ' + res.data[0].tem1 + '/' + res.data[0].tem2 + ' ' + res.data[0].air_level);
         //今日天气
-        $('.wea_top').append('<span class="city"><b>' + res.city + '</b> ' + res.update_time + '更新</span><span class="img" style="background:url(https://xuesax.com/tianqiapi/skin/peach/' + res.data[0].wea_img + '.png) no-repeat center/contain;"></span> <span class="tem"><b>' + res.data[0].tem + '</b>' + res.data[0].wea + '</span><span class="air">空气质量：' + res.data[0].air_level + '<br>湿度：' + res.data[0].humidity + '%<br>' + res.data[0].win[0] + '：' + res.data[0].win_speed + '</span><span class="air_tips">' + res.data[0].air_tips + '</span>');
+        $('.wea_top').append('<span class="city"><b>' + res.city + '</b> ' + res.update_time + '更新</span><span class="img" style="background:url(https://www.tianqiapi.com/static/skin/yahoo/' + res.data[0].wea_img + '.png) no-repeat center/contain;"></span> <span class="tem"><b>' + res.data[0].tem + '</b>' + res.data[0].wea + '</span><span class="air">空气质量：' + res.data[0].air_level + '<br>湿度：' + res.data[0].humidity + '%<br>' + res.data[0].win[0] + '：' + res.data[0].win_speed + '</span><span class="air_tips">' + res.data[0].air_tips + '</span>');
         //今日指数
         $('.wea_con ul').append('<li>' + res.data[0].index[0].title + '<br>' + res.data[0].index[0].level + '</b><br><i>' + res.data[0].index[0].desc + '</i></li>');
         $('.wea_con ul').append('<li>' + res.data[0].index[3].title + '<br>' + res.data[0].index[3].level + '</b><br><i>' + res.data[0].index[3].desc + '</i></li>');
         $('.wea_con ul').append('<li>' + res.data[0].index[4].title + '<br>' + res.data[0].index[4].level + '</b><br><i>' + res.data[0].index[4].desc + '</i></li>');
-        $('.wea_con ul').append('<li>' + res.data[0].index[5].title + '<br>' + res.data[0].index[5].level + '</b><br><i>' + res.data[0].index[5].desc + '</i></li>');
+        $('.wea_con ul').append('<li>空气污染<br>' + res.data[0].index[5].level + '</b><br><i>' + res.data[0].index[5].desc + '</i></li>');
 
         //未来四天天气
-        $('.wea_foot ul').append('<li>' + res.data[1].day + '<br><img src="https://xuesax.com/tianqiapi/skin/peach/' + res.data[1].wea_img + '.png"><br><b>' + res.data[1].wea + '</b><br><i>' + res.data[1].tem1 + '°/' + res.data[1].tem2 + '°' + '</i></li>');
-        $('.wea_foot ul').append('<li>' + res.data[2].day + '<br><img src="https://xuesax.com/tianqiapi/skin/peach/' + res.data[2].wea_img + '.png"><br><b>' + res.data[2].wea + '</b><br><i>' + res.data[2].tem1 + '°/' + res.data[2].tem2 + '°' + '</i></li>');
-        $('.wea_foot ul').append('<li>' + res.data[3].day + '<br><img src="https://xuesax.com/tianqiapi/skin/peach/' + res.data[3].wea_img + '.png"><br><b>' + res.data[3].wea + '</b><br><i>' + res.data[3].tem1 + '°/' + res.data[3].tem2 + '°' + '</i></li>');
+        $('.wea_foot ul').append('<li>' + res.data[1].day + '<br><img src="https://www.tianqiapi.com/static/skin/yahoo/' + res.data[1].wea_img + '.png"><br><b>' + res.data[1].wea + '</b><br><i>' + res.data[1].tem1 + '°/' + res.data[1].tem2 + '°' + '</i></li>');
+        $('.wea_foot ul').append('<li>' + res.data[2].day + '<br><img src="https://www.tianqiapi.com/static/skin/yahoo/' + res.data[2].wea_img + '.png"><br><b>' + res.data[2].wea + '</b><br><i>' + res.data[2].tem1 + '°/' + res.data[2].tem2 + '°' + '</i></li>');
+        $('.wea_foot ul').append('<li>' + res.data[3].day + '<br><img src="https://www.tianqiapi.com/static/skin/yahoo/' + res.data[3].wea_img + '.png"><br><b>' + res.data[3].wea + '</b><br><i>' + res.data[3].tem1 + '°/' + res.data[3].tem2 + '°' + '</i></li>');
     }
 
 });
