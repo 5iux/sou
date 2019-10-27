ab39d9f5a14532521c9e39219e659cf9490a37d8
likun <1617455243@qq.com>
2019-10-27 16:47:34 +0800

    fix: js页面天气数据获取修复

--- sou02.js
+++ sou02.js
@@ -63,31 +63,108 @@ $(document).ready(function() {
 天气插件api请地址：www.tianqiapi.com
 */
 /*天气插件开始*/
-$.ajax({
-    url: 'https://www.tianqiapi.com/api/',
-    data: 'version=v1&city=',
-    dataType: 'JSON',
-    error: function() {
-        console.log('天气插件网络错误！');
-    },
-    success: function(res) {
-        uptime = res.update_time.substring(11);
-        uptime = uptime.substring(0, uptime.length - 3);
-        $('.wea_hover').css('background-image', 'url(/sou/wea/icon/bg/100d.png)');
-        $('.mywth').append(res.city + ' <img class="wea" src="https://www.tianqiapi.com/static/skin/yahoo/' + res.data[0].wea_img + '.png"> ' + res.data[0].wea + ' ' + res.data[0].tem1 + '/' + res.data[0].tem2 + ' ' + res.data[0].air_level);
-        //今日天气
-        $('.wea_top').append('<span class="city"><b>' + res.city + '</b> ' + res.update_time + '更新</span><span class="img" style="background:url(https://www.tianqiapi.com/static/skin/yahoo/' + res.data[0].wea_img + '.png) no-repeat center/contain;"></span> <span class="tem"><b>' + res.data[0].tem + '</b>' + res.data[0].wea + '</span><span class="air">空气质量：' + res.data[0].air_level + '<br>湿度：' + res.data[0].humidity + '%<br>' + res.data[0].win[0] + '：' + res.data[0].win_speed + '</span><span class="air_tips">' + res.data[0].air_tips + '</span>');
-        //今日指数
-        $('.wea_con ul').append('<li>' + res.data[0].index[0].title + '<br><b>' + res.data[0].index[0].level + '</b><br><i>' + res.data[0].index[0].desc + '</i></li>');
-        $('.wea_con ul').append('<li>' + res.data[0].index[3].title + '<br><b>' + res.data[0].index[3].level + '</b><br><i>' + res.data[0].index[3].desc + '</i></li>');
-        $('.wea_con ul').append('<li>' + res.data[0].index[4].title + '<br><b>' + res.data[0].index[4].level + '</b><br><i>' + res.data[0].index[4].desc + '</i></li>');
-        $('.wea_con ul').append('<li>空气污染<br><b>' + res.data[0].index[5].level + '</b><br><i>' + res.data[0].index[5].desc + '</i></li>');
-
-        //未来四天天气
-        $('.wea_foot ul').append('<li>' + res.data[1].day + '<br><img src="https://www.tianqiapi.com/static/skin/yahoo/' + res.data[1].wea_img + '.png"><br><b>' + res.data[1].wea + '</b><br><i>' + res.data[1].tem1 + '°/' + res.data[1].tem2 + '°' + '</i></li>');
-        $('.wea_foot ul').append('<li>' + res.data[2].day + '<br><img src="https://www.tianqiapi.com/static/skin/yahoo/' + res.data[2].wea_img + '.png"><br><b>' + res.data[2].wea + '</b><br><i>' + res.data[2].tem1 + '°/' + res.data[2].tem2 + '°' + '</i></li>');
-        $('.wea_foot ul').append('<li>' + res.data[3].day + '<br><img src="https://www.tianqiapi.com/static/skin/yahoo/' + res.data[3].wea_img + '.png"><br><b>' + res.data[3].wea + '</b><br><i>' + res.data[3].tem1 + '°/' + res.data[3].tem2 + '°' + '</i></li>');
-    }
-
-});
+const indexs = {
+    comf: "舒适度",
+    drsg: "穿衣",
+    flu: "感冒",
+    sport: "运动",
+    trav: "旅游",
+    uv: "紫外线",
+    cw: "洗车",
+    air: "空气"
+  };
+  // key自己上https://dev.heweather.com申请
+  Promise.all(
+    ["now", "forecast", "lifestyle"].map(weatherType =>
+      fetch(
+        `https://free-api.heweather.net/s6/weather/${weatherType}?key=2ed27772494e4b038f6214779e7ff9aa&location=auto_ip`
+      ).then(res => res.json())
+    )
+  )
+    .then(res => ({
+      ...res[0].HeWeather6[0],
+      ...res[1].HeWeather6[0],
+      ...res[2].HeWeather6[0]
+    }))
+    .then(res => {
+      const { update, basic, daily_forecast, now, lifestyle } = res;
+      uptime = update.utc.substring(11);
+      uptime = uptime.substring(0, uptime.length - 3);
+      $(".wea_hover").css(
+        "background-image",
+        `url(/wea/icon/bg/${now.cond_code}d.png)`
+      );
+      $(".mywth").append(
+        `${basic.location} <img class="wea" src="./wea/icon/${now.cond_code}d.png"> ${now.cond_txt} ${now.tmp}℃ ${now.wind_dir}`
+      );
+      //今日天气
+      $(".wea_top").append(
+        `<span class="city"><b>${basic.location}</b> ${update.utc}更新</span><span class="img" style="background:url(/wea/icon/${now.cond_code}d.png) no-repeat center/contain;"></span> <span class="tem"><b>${now.tmp}</b>${now.cond_txt}
+          </span><span class="air">能见度：${now.vis}公里<br>相对湿度：${now.hum}%<br>${now.wind_dir}：${now.wind_sc}</span><span class="air_tips">${lifestyle[Math.floor(Math.random() * lifestyle.length)].txt}</span>
+          `
+      );
+      //今日指数
+      $(".wea_con ul").append(
+        lifestyle.map(
+          life => `
+              <li>${indexs[life.type]}<br><b>${life.brf}</b><br><i>${
+            life.txt
+          }</i></li>
+          `
+        )
+      );
+      //未来三天天气
+      $(".wea_foot ul").append(
+        daily_forecast.map(
+          day => `
+              <li>${day.date}<br><img src="/wea/icon/${day.cond_code_d}d.png" /><br><b>
+              ${day.cond_txt_d}</b><br><i>
+              ${day.tmp_min}°/${day.tmp_max}°
+              </i></b></li>
+          `
+        )
+      );
+      $(".wea_foot ul").append(
+        "<li>" +
+          res.data[1].day +
+          '<br><img src="https://www.tianqiapi.com/static/skin/yahoo/' +
+          res.data[1].wea_img +
+          '.png"><br><b>' +
+          res.data[1].wea +
+          "</b><br><i>" +
+          res.data[1].tem1 +
+          "°/" +
+          res.data[1].tem2 +
+          "°" +
+          "</i></li>"
+      );
+      $(".wea_foot ul").append(
+        "<li>" +
+          res.data[2].day +
+          '<br><img src="https://www.tianqiapi.com/static/skin/yahoo/' +
+          res.data[2].wea_img +
+          '.png"><br><b>' +
+          res.data[2].wea +
+          "</b><br><i>" +
+          res.data[2].tem1 +
+          "°/" +
+          res.data[2].tem2 +
+          "°" +
+          "</i></li>"
+      );
+      $(".wea_foot ul").append(
+        "<li>" +
+          res.data[3].day +
+          '<br><img src="https://www.tianqiapi.com/static/skin/yahoo/' +
+          res.data[3].wea_img +
+          '.png"><br><b>' +
+          res.data[3].wea +
+          "</b><br><i>" +
+          res.data[3].tem1 +
+          "°/" +
+          res.data[3].tem2 +
+          "°" +
+          "</i></li>"
+      );
+    });
 /*天气插件结束*/
